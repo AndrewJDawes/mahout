c669769e4ae37f9df19dd1ca1536e1db79938bf0
Andrew <AndrewJamesDawes@gmail.com>
2021-07-25 17:25:52 -0400

    Add install script for dependencies

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# DEPENDENCIES
+
+# EXPORT EVERNOTE TO .enex
+# https://github.com/brokensandals/exporteer_evernote_osx
+pip3 install exporteer_evernote_osx
+
+# CONVERT .enex to .md
+# https://github.com/wormi4ok/evernote2md
+brew install evernote2md
+
+# IMPORT JSON TO Simplenote
+# https://github.com/insanum/sncli
+pip3 install sncli
